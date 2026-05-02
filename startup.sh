a0a1b726be47dc1afbc866b108a6d8c963782ffe
Sadananda Paul <sp3629@srmist.edu.in>
2026-05-03 04:55:38 +0530

    Install dependencies before starting Next.js
    Added dependency installation step before starting Next.js.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "=== Startup script begin ==="
-
 cd /home/site/wwwroot
+
+echo "=== Installing dependencies ==="
+npm install --omit=dev --prefer-offline
 
 echo "=== Starting Next.js ==="
 npm start
