37f1d68919a15303ae3fa631be315c7de48d3db6
Sadananda Paul <sp3629@srmist.edu.in>
2026-05-03 09:39:53 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 echo "=== Startup script begin ==="
 cd /home/site/wwwroot
-
-if [ -f "node_modules.tar.gz" ] && [ ! -f "node_modules/.bin/next" ]; then
-  echo "=== Extracting node_modules ==="
-  tar -xzf node_modules.tar.gz
-fi
-
 echo "=== Starting Next.js ==="
-npm start
+node server.js
