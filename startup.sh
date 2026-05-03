60ecb8579125f078a0029e8dc44e36b781cbfae5
Sadananda Paul <sp3629@srmist.edu.in>
2026-05-03 10:18:37 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "=== Startup script begin ==="
 cd /home/site/wwwroot
-
 echo "=== Starting Next.js ==="
-chmod +x node_modules/.bin/next
-node_modules/.bin/next start
+node server.js
